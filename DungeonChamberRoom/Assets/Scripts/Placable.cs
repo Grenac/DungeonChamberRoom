b6fd92247129ba651d639f38f9adfde057c9cdab
Grenac <ivor.grenac@gmail.com>
2016-07-09 14:27:22 +0200

    Opet nešto slatko

---
+++ DungeonChamberRoom/Assets/Scripts/Placable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Placable : MonoBehaviour {
+
+
+	private Vector2 worldPosition;
+	public Vector2 WorldPosition {
+		get {
+			return worldPosition;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		worldPosition = new Vector2(2, 4);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
---
+++ DungeonChamberRoom/Assets/Scripts/WorldController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldController : MonoBehaviour {
+
+
+	private List<GameObject> chamberObjects;
+
+	public void registerChamberObject(GameObject newObject) {
+		chamberObjects.Add(newObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		chamberObjects = new List<GameObject>();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(chamberObjects);
+	}
+}
